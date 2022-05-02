c59d76f59a5282164eaf646eb5ba488eeb7399de
Artem Tseranu <artem.tseranu@xing.com>
2022-05-03 00:49:55 +0200

    Set up org with GTD capture templates

--- init.el
+++ init.el
@@ -1,4 +1,7 @@
-(add-to-list 'load-path (expand-file-name "~/emacs_config"))
+(defconst my/ROOT (if load-file-name
+                      (file-name-directory load-file-name)))
+
+(add-to-list 'load-path my/ROOT)
 
 (define-derived-mode my/init-mode emacs-lisp-mode "Init" "Major mode for init files.")
 (add-to-list 'auto-mode-alist '("\\.init\\'" . my/init-mode))
